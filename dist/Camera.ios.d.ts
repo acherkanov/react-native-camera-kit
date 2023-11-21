dd8a65c7b12fa57d785d0c0637e11dbf49b72d7c
acherkanov <135016183+acherkanov@users.noreply.github.com>
2023-11-21 15:13:09 +0300

    add dist

---
+++ dist/Camera.ios.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { CameraProps } from './Camera';
+declare const Camera: React.ForwardRefExoticComponent<Pick<CameraProps, "style" | "flashMode" | "focusMode" | "zoomMode" | "zoom" | "maxZoom" | "torchMode" | "cameraType" | "onOrientationChange" | "onZoom" | "onError" | "scanBarcode" | "showFrame" | "laserColor" | "frameColor" | "onReadCode" | "ratioOverlay" | "ratioOverlayColor" | "resetFocusTimeout" | "resetFocusWhenMotionDetected" | "scanThrottleDelay" | "shutterPhotoSound"> & React.RefAttributes<unknown>>;
+export default Camera;
